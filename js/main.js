80ea0169da2aa86ba6e7fcd37fb5326bf93f420e
vitalydok <89067055+vitalydok@users.noreply.github.com>
2021-08-20 21:29:06 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@ $(document).ready(function () {
     var floorPath = $(".home-image path");
     var counterUp = $(".counter-up");
     var counterDown = $(".counter-down");
+    var modal = $('.modal');
+    var modalCloseButton = $('.modal-close-button');
+    var currentFlats = 1; 
+    var flatsPath = $(".flats path"); 
+    var flatsPathItem = $(".flat-item a"); 
+    var viewFlats = $(".view-flats");
+
     floorPath.on("mouseover", function() {
         floorPath.removeClass("current-floor");
         currentFloor = $(this).attr("data-floor");
@@ -19,6 +26,11 @@ $(document).ready(function () {
         }
     });
 
+    floorPath.on('click', toggleModal);
+
+    viewFlats.on('click', toggleModal);
+
+    modalCloseButton.on('click', toggleModal);
 
     counterDown.on('click', function(){
         if (currentFloor > 2) {
@@ -28,5 +40,27 @@ $(document).ready(function () {
             floorPath.removeClass("current-floor");
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor");
         }
-    })
+    });
+
+    function toggleModal () {
+        modal.toggleClass("is-open");
+    };
+
+    flatsPath.on('mouseover', function () {
+        currentFlats = $(this).attr("data-flats");
+        flatsPath.removeClass("current-flats");
+        flatsPathItem.removeClass("current-flats-item");
+        $(`[data-flats=${currentFlats}]`).toggleClass("current-flats");
+        $(`[data-item=${currentFlats}]`).toggleClass("current-flats-item");
+    });
+
+    flatsPathItem.on('mouseover', function () {
+        currentFlats = $(this).attr("data-item");
+        flatsPath.removeClass("current-flats");
+        flatsPathItem.removeClass("current-flats-item"); 
+        $(`[data-flats=${currentFlats}]`).toggleClass("current-flats"); 
+        $(`[data-item=${currentFlats}]`).toggleClass("current-flats-item"); 
+    });
+
+
 });
